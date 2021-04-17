89ff72f3acb75f0ddf3d78d2988d8b8b254e1033
digenets <digenets@gmail.com>
2021-04-17 17:34:19 +0300

    added list of file's pointers and function for recommenting(может не работать)

--- main.c
+++ main.c
@@ -1,1 +1,25 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_HEADER_NAMESIZE 30
+void file_open(int n_headers, char** argv){
+	n_headers--;
+	
+	char** filenames  = (char**) malloc(n_headers * sizeof(char*));
+	FILE** incl_files = (FILE**) malloc(n_headers * sizeof(FILE*));
+
+	for (unsigned int i = 0; i < n_headers; ++i){
+		// incl_files[i] = (FILE*) malloc(sizeof(FILE));
+		incl_files[i] = fopen(argv[i+1], "wt");
+		if (incl_files[i] == NULL){
+			printf("File open error");
+			break;
+		}
+	}
+}
+
+int main(int argc, char ** argv)
+{
+	file_open(argc, argv);
+	return 0;
+}
--- recommenting.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-#include <stdbool.h>
-
-void recomment(FILE* input, FILE * output);
