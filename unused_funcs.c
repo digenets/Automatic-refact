ea2226409cea8964c63ffec28d015007bda43f67
digenets <digenets@gmail.com>
2021-05-03 16:18:44 +0300

    commit2

---
+++ unused_funcs.c
@@ -0,0 +1,5 @@
+#include <unused_func>
+
+UNUSED_LIST remufv(FILE* input){ // the function returns a structure that contains arrays of lists with the names of unused functions and variables
+	
+}
---
+++ unused_funcs.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct UNUSED_LIST
+{
+	char **func_list;
+	char **var_list;
+};
